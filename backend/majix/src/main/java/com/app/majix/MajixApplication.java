8f7f5f4912fd37565ff20a0ce521249749504009
LAB24_USER ON WS36 <L24Y10W36@LabHCI.net>
2025-10-28 13:19:07 +0800

    setup(database): Added cart_cartitem_return_product_tables

--- backend/majix/src/main/java/com/app/majix/MajixApplication.java
+++ backend/majix/src/main/java/com/app/majix/MajixApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MajixApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MajixApplication.class, args);
-	}
+		public static void main(String[] args) {
+			SpringApplication.run(MajixApplication.class, args);
+		}
+
+
 
 }
