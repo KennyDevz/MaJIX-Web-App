4f8bf1746d9affd5ca0038ad7b23a624549d2f9e
LAB24_USER ON WS33 <L24Y10W33@LabHCI.net>
2025-10-28 11:16:35 +0800

    setup/majix/backend_setup

---
+++ backend/majix/src/main/java/com/app/majix/MajixApplication.java
@@ -0,0 +1,13 @@
+package com.app.majix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MajixApplication.class, args);
+	}
+
+}
