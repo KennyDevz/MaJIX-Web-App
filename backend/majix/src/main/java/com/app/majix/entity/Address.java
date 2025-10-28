cf5aafeba1c5554aa7ef510f48a38de38838d8c0
LAB24_USER ON WS33 <L24Y10W33@LabHCI.net>
2025-10-28 12:58:30 +0800

    setup(database): database tables for payment, orders, orderitem, and address

---
+++ backend/majix/src/main/java/com/app/majix/entity/Address.java
@@ -0,0 +1,80 @@
+package com.app.majix.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long addressId;
+
+    private String street;
+    private String city;
+    private String province;
+    private String country;
+    private int postalCode;
+
+    // --- Constructors ---
+    public Address() {}
+
+    public Address(String street, String city, String province, String country, int postalCode) {
+        this.street = street;
+        this.city = city;
+        this.province = province;
+        this.country = country;
+        this.postalCode = postalCode;
+    }
+
+    // --- Getters and Setters ---
+    public Long getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Long addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+}
