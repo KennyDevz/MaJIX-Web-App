2ecbec8d0088babf8e48cb24c72683b5d1b43c52
Mechole17 <232763660+Mechole17@users.noreply.github.com>
2025-11-09 00:25:03 +0800

    feature(navbar): updated navbar and route to profile page

--- frontend/src/components/nav_bar.js
+++ frontend/src/components/nav_bar.js
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom"
 import CartIcon from "../assets/cart-icon.png"
 import SearchBar from './searchbar'
 import { UserContext } from "../context/UserContext"
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 
 function NavBar(){
     const { user, setUser } = useContext(UserContext);
+    const [showDropDown, setShowDropDown] = useState(false)
     const navigate = useNavigate()
+
     return(
         <div className="nav-bar">
             <h1><Link to="/" className="route-link">MaJIX</Link></h1>
@@ -25,21 +27,33 @@ function NavBar(){
                 <img src={CartIcon} alt="cart-icon"/>
             </Link>
 
+            { user ? (
+             <div className="relative">
+                {/* Profile Button */}
+                
+                <div
+                onClick={() => setShowDropDown(!showDropDown)}
+                className="rounded-full bg-gray-700 flex items-center justify-center hover:bg-gray-600"
+                style={{borderRadius: '40%', cursor: 'pointer', border: '1px solid black', padding: '5px'}}
+                >
+                👤
+                </div>
+                
 
+                {/* Logout Modal */}
+                {showDropDown && (
+                <div style={{display: 'flex', flexDirection: 'column'}}>
+                    <Link to="/Profile" className="route-link">Profile</Link>
+                </div>
+                )}
+            
+            </div>
+            ):(
              <ul className="user-links">
                 <li><Link to="/Sign-in" className="route-link">Sign In</Link></li>
                 <li><Link to="/Sign-up" className="route-link">Sign Up</Link></li>
              </ul>
-             {user && 
-             <button onClick={()=> {
-                setUser(null); 
-                alert("Logged Out");
-                localStorage.removeItem("user");
-                navigate("/Sign-in",{replace: true})
-            }}
-            >LOGOUT</button>
-
-             }
+            )}
              
         </div>
     )
